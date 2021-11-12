a433e9510587de47417a7e0a86a427b90d4aab93
Your Name <matt14w@gmail.com>
2021-11-12 13:09:52 -0500

    mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,12 +12,26 @@ var enemyAttack = 12;
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-enemyHealth = enemyHealth-playerAttack
+playerHealth = playerHealth - enemyAttack
   // Log a resulting message to the console so we know that it worked.
-  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+console.log( playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
-  // Log a resulting message to the console so we know that it worked.
+//check players health
+if (playerHealth <=0) {
+  window.alert(playername + " has died!");
+}
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+//check enemy health
+
+if (enemyHealth<=0) {
+  window.alert (enemyName + " has died!");
+}
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
 
 
 //Testing commit
