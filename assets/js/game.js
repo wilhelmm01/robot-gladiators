a36d1c02fe1127f93c0fd2237573db681bf12d7c
Your Name <matt14w@gmail.com>
2021-11-13 19:41:34 -0500

    multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,15 +4,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -68,4 +65,8 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+// fight();
+
+for(var i=0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
